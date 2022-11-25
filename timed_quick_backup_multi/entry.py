8764c825e7c2cd5e4b64cb1f477c8c27dcb2481c
Fallen_Breath <yqx791125833@gmail.com>
2022-11-25 18:29:26 +0800

    make things nice

--- timed_quick_backup_multi/entry.py
+++ timed_quick_backup_multi/entry.py
@@ -17,19 +17,35 @@ class Config(Serializable):
 config: Config
 CONFIG_FILE = os.path.join('config', 'timed_quick_backup_multi.json')
 clock = None  # type: Optional[TimedQBM]
-playercount = 0
-flag_countright = False
+player_count = 0
+flag_count_correct = False
 
 
 def save_config():
 	stored.server.save_config_simple(config, CONFIG_FILE, in_data_folder=False)
 
-def online_player_check(count): #is the playercount right?(correct playercount and someone is online?)
-	global flag_countright
-	if (flag_countright) and (count > 1):
+
+def has_online_player():
+	if flag_count_correct and player_count >= 1:
 		return True
 	else:
 		return False
+
+
+def player_online_clock_state_sync():
+	if config.require_online_players:
+		if flag_count_correct:
+			clock.set_enabled(has_online_player())
+		else:
+			clock.set_enabled(False)
+
+
+def set_player_count(count: int):
+	if config.require_online_players:
+		if flag_count_correct:  # only maintain the player count if the flag is true
+			global player_count
+			player_count = count
+			player_online_clock_state_sync()
 
 
 # ------------------
@@ -59,8 +75,19 @@ def reset_timer(source: CommandSource):
 	clock.broadcast_next_backup_time()
 
 
+def show_status(source: CommandSource):
+	source.reply(TimedQBM.tr('status.config_enabled', config.enabled))
+	if config.enabled:
+		source.reply(TimedQBM.tr('status.clock_enabled', clock.is_enabled))
+		if clock.is_enabled:
+			source.reply(TimedQBM.tr('status.clock_interval', round(config.interval, 2)))
+			source.reply(clock.get_next_backup_message())
+		if config.require_online_players:
+			source.reply(TimedQBM.tr('status.has_online_player', has_online_player()))
+
+
 def register_things(server: PluginServerInterface):
-	HELP_MESSAGE = TimedQBM.tr('help_message', constants.PREFIX, stored.metadata.name, stored.metadata.version, stored.metadata.get_description(server.get_mcdr_language()))
+	HELP_MESSAGE = TimedQBM.tr('help_message', constants.PREFIX, stored.metadata.name, stored.metadata.version, stored.metadata.get_description_rtext())
 	server.register_event_listener(constants.BACKUP_DONE_EVENT, lambda svr, src, slot_info: clock.on_backup_created(slot_info))
 	server.register_help_message(constants.PREFIX, TimedQBM.tr('info'), permission=config.permission_requirement)
 	server.register_command(
@@ -72,7 +99,8 @@ def register_things(server: PluginServerInterface):
 		then(Literal('enable').runs(lambda src: set_enabled(src, True))).
 		then(Literal('disable').runs(lambda src: set_enabled(src, False))).
 		then(Literal('set_interval').then(Float('interval').at_min(0.1).runs(lambda src, ctx: set_interval(src, ctx['interval'])))).
-		then(Literal('reset_timer').runs(reset_timer))
+		then(Literal('reset_timer').runs(reset_timer)).
+		then(Literal('status').runs(show_status))
 	)
 
 
@@ -80,61 +108,46 @@ def register_things(server: PluginServerInterface):
 #   MCDR Events
 # ---------------
 
-
 def on_load(server: PluginServerInterface, prev):
 	global config
 	stored.server = server
 	stored.metadata = server.get_self_metadata()
 	config = server.load_config_simple(CONFIG_FILE, target_class=Config, in_data_folder=False)
 
-	global clock, playercount
+	global clock, player_count, flag_count_correct
 	clock = TimedQBM(server)
 	try:
 		clock.time_since_backup = float(prev.clock.time_since_backup)
-		playercount = prev.playercount
+		player_count = int(prev.player_count)
+		flag_count_correct = bool(prev.flag_count_correct)
 	except (AttributeError, ValueError):
 		pass
 
-	if (playercount != None) and (online_player_check(playercount)):
-		clock.set_enabled(config.enabled)
-	else:
-		clock.set_enabled(False)
+	clock.set_enabled(config.enabled)
+	player_online_clock_state_sync()
 	clock.start()
 
 	register_things(server)
 
 
 def on_server_start(server: PluginServerInterface):
-	#disable backup if online player is required until someone joins the server
-	#putting logic here to avoid unintended behavior from reload/load plugin after server started
-	global config, clock, flag_countright
-	flag_countright = True #yeah we are loaded before minecraft server, thus playercount is right!
+	"""
+	disable backup if online player is required until someone joins the server
+	putting logic here to avoid unintended behavior from reload/load plugin after server started
+	"""
+	global config, clock, flag_count_correct
 	if config.require_online_players:
-		clock.set_enabled(False)
-	else:
-		clock.set_enabled(config.enabled)
+		server.logger.info('Server start detected, enabling online player requirement testing')
+	flag_count_correct = True  # yeah we are loaded before minecraft server, thus playercount is right!
+	set_player_count(0)
 
 
 def on_player_joined(server: PluginServerInterface, player: str, info: Info):
-	global config
-
-	if config.require_online_players:
-		global playercount, clock
-		if online_player_check(playercount):
-			playercount += 1
-			clock.set_enabled(config.enabled)
-		else:
-			playercount += 1
+	set_player_count(player_count + 1)
 
 
-def on_player_left(server: PluginServerInterface, player: str, info: Info):
-	global config
-
-	if config.require_online_players:
-		global playercount, clock
-		playercount -= 1
-		if not online_player_check(playercount):
-			clock.set_enabled(False)
+def on_player_left(server: PluginServerInterface, info: Info):
+	set_player_count(player_count - 1)
 
 
 def on_unload(server):
